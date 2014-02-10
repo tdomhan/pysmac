ce32c9f683b9fa0a921645ad9c5ee3c68c49dfcf
Tobias Domhan <tdomhan@gmail.com>
2014-02-10 14:33:18 +0100

    moved to package structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name = "pysmac",
+    version = "0.1",
+    packages = find_packages(),
+    install_requires = ['numpy', 'docutils>=0.3'],
+    author = "Tobias Domhan",
+    author_email = "domhant@informatik.uni-freiburg.de",
+    description = "python interface to SMAC.",
+    test_suite = 'pysmac.test.smacparse',
+    keywords = "hyperparameter optimization hyperopt bayesian smac",
+    url = "http://www.cs.ubc.ca/labs/beta/Projects/SMAC/"
+)
