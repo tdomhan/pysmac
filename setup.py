07d3351b9d9e0a54e88d2add135d8bb426ac6740
Tobias Domhan <tdomhan@gmail.com>
2014-05-26 18:21:54 +0200

    checking java version

--- setup.py
+++ setup.py
@@ -7,6 +7,23 @@ from setuptools import setup, find_packages
         http://johnmacfarlane.net/pandoc/try/
 """
 
+def check_java_version():
+    import re
+    from subprocess import STDOUT, check_output
+    out = check_output(["java", "-version"], stderr=STDOUT).split("\n")
+    if len(out) < 1:
+        print "failed checking Java version. Make sure Java version 7 or greater is installed."
+        return False
+    m = re.match('java version "\d+.(\d+)..*', out[0])
+    if m is None or len(m.groups()) < 1:
+        print "failed checking Java version. Make sure Java version 7 or greater is installed."
+        return False
+    java_version = int(m.group(1))
+    if java_version < 7:
+        error_msg = "Found Java version %d, but Java version 7 or greater is required." % java_version
+ 
+        raise RuntimeError(error_msg)
+
 def check_java_exists():
     from subprocess import call
     import os
@@ -23,6 +40,7 @@ def check_java_exists():
         raise RuntimeError(error_msg)
 
 check_java_exists()
+check_java_version()
 
 setup(
     name = "pysmac",
